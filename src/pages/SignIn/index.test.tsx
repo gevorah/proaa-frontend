b692518f2c82847dadfbde569b5f64ec0b3d6257
gevorah <jhon.ijaji@encora.com>
2023-09-25 10:55:34 -0500

    feat: sign in test

--- src/pages/SignIn/index.test.tsx
+++ src/pages/SignIn/index.test.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 import SignIn from './'
 
 describe('Sign In Page', () => {
   it('should render the sign-in page', async () => {
     const component = render(<SignIn />)
+
     expect(component.getByText('Sign in')).toBeTruthy()
+    expect(component.getByPlaceholderText('Email')).toBeTruthy()
+    expect(component.getByPlaceholderText('Password')).toBeTruthy()
+  })
+
+  it('should show errors when the form is submitted without data', async () => {
+    const component = render(<SignIn />)
+
+    const button = component.getByRole('button')
+    fireEvent.click(button)
+
+    expect(await component.findByText('Email is required')).toBeTruthy()
+    expect(await component.findByText('Password is required')).toBeTruthy()
   })
 })
--- src/pages/SignUp/index.test.tsx
+++ src/pages/SignUp/index.test.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 import SignUp from './'
 
 describe('Sign Up Page', () => {
   it('should render the sign-up page', async () => {
     const component = render(<SignUp />)
+
     expect(component.getByText('Sign up')).toBeTruthy()
+    expect(component.getByPlaceholderText('Email')).toBeTruthy()
+    expect(component.getByPlaceholderText('Password')).toBeTruthy()
+  })
+
+  it('should show errors when the form is submitted without data', async () => {
+    const component = render(<SignUp />)
+
+    const button = component.getByRole('button')
+    fireEvent.click(button)
+
+    expect(await component.findByText('Email is required')).toBeTruthy()
+    expect(await component.findByText('Password is required')).toBeTruthy()
   })
 })
