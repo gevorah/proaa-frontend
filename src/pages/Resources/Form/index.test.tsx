033a0f543daa0c0717fbf4cca0ce4f08a844258f
gevorah <jhon.ijaji@encora.com>
2023-10-19 17:04:38 -0500

    feat: test edit resource

--- src/pages/Resources/Form/index.test.tsx
+++ src/pages/Resources/Form/index.test.tsx
@@ -7,12 +7,13 @@ import { topic } from '@/test-setup'
 import ResourcePage from '../Form'
 
 const router = vi.hoisted(() => ({
-  useNavigate: vi.fn()
+  useNavigate: vi.fn(),
+  useParams: vi.fn(() => ({ id: '1' }))
 }))
 
 vi.mock('react-router-dom', () => router)
 
-describe('ResourcePage', () => {
+describe('Resource Page', () => {
   describe('Create Mode', () => {
     it('should render the resource page in create mode', async () => {
       const component = render(<ResourcePage mode="create" />)
@@ -36,7 +37,7 @@ describe('ResourcePage', () => {
       expect(await component.findByText('Topic is required')).toBeTruthy()
     })
 
-    it('should show error when the form is submitted with invalid topic', async () => {
+    it('should show error when the form is submitted with invalid resource', async () => {
       const component = render(<ResourcePage mode="create" />)
 
       const descriptionField = component.getByPlaceholderText('Description')
@@ -45,15 +46,58 @@ describe('ResourcePage', () => {
       const urlField = component.getByPlaceholderText('Url')
       await userEvent.type(urlField, 'http://test.com')
 
-      const topicField = component.getByRole('combobox')
-      const topicOption = component.getByRole('option', { name: topic.name })
-      topicOption.setAttribute('value', '10')
-      await userEvent.selectOptions(topicField, topicOption)
+      const resourceField = component.getByRole('combobox')
+      const resourceOption = component.getByRole('option', { name: topic.name })
+      resourceOption.setAttribute('value', '10')
+      await userEvent.selectOptions(resourceField, resourceOption)
 
       const button = component.getByRole('button')
       await userEvent.click(button)
 
       expect(await component.findByText('Topic is not valid')).toBeTruthy()
+    })
+  })
+
+  describe('Edit Mode', () => {
+    it('should render the resource page in edit mode', async () => {
+      const component = render(<ResourcePage mode="edit" />)
+
+      expect(component.getByPlaceholderText('Description')).toBeTruthy()
+      expect(component.getByPlaceholderText('Url')).toBeTruthy()
+      expect(
+        component.getByRole('option', { name: 'Select a Topic' })
+      ).toBeTruthy()
+    })
+
+    it('should show error page when id is not valid', async () => {
+      router.useParams.mockReturnValueOnce({ id: 'x' })
+
+      const component = render(<ResourcePage mode="edit" />)
+
+      expect(await component.findByText('Resource Not Found')).toBeTruthy()
+    })
+
+    it('should show errors when the form is submitted without data', async () => {
+      const component = render(<ResourcePage mode="edit" />)
+
+      const descriptionField = component.getByPlaceholderText('Description')
+      await userEvent.type(descriptionField, '{Backspace>99}')
+
+      const urlField = component.getByPlaceholderText('Url')
+      await userEvent.type(urlField, '{Backspace>99}')
+
+      const resourceField = component.getByRole('combobox')
+      const resourceOption = component.getByRole('option', {
+        name: 'Select a Topic'
+      })
+      await userEvent.selectOptions(resourceField, resourceOption)
+
+      const button = component.getByRole('button')
+      await userEvent.click(button)
+
+      expect(await component.findByText('Description is required')).toBeTruthy()
+      expect(await component.findByText('Url is required')).toBeTruthy()
+      expect(await component.findByText('Topic is required')).toBeTruthy()
     })
   })
 })
